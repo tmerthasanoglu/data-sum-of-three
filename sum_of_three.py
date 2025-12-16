e3aa5b9a79c0e28eba9d25cbc32ed25a1e49e075
Mert <temelmert53@gmail.com>
2025-12-16 11:56:36 +0300

    Solve first Python exercise of the day

--- sum_of_three.py
+++ sum_of_three.py
@@ -1,7 +1,5 @@
-# pylint: disable=missing-module-docstring
-
 def sum3(first_number, second_number, third_number):
     """
     Takes three numbers and returns the sum of those numbers
     """
-    pass  # YOUR CODE HERE
+    return first_number + second_number + third_number
